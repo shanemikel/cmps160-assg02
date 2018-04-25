f92d08a49fe2f822eb84db382662fbd50ef753a1
Shane Pearlman <shanemikel1@gmail.com>
2018-04-25 13:55:20 -0700

    Start integration of polylines assg into new interface

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
-MAIN      := driver proto ngon2d
-JS_LIBS   := shapes
+MAIN      := driver
+JS_LIBS   :=
 LESS_LIBS := palette
 HTM_LIBS  := head-start base-styles jquery font-awesome head-end body-start
 HTM_LIBS  += webgl-libs body-end
--- driver.js
+++ driver.js
@@ -1,14 +1,134 @@
-#include "shapes.js"
+#define WHITE [1.0, 1.0, 1.0, 1.0]
 
-#define POINT_SIZE 8
+var RED   = null;
+var GREEN = null;
+var BLUE  = null;
 
+var g_reinit_lock = false;
+var g_canvas      = null;
+
+var WIDTH  = null;
+var HEIGHT = null;
+
+function init() {
+    $(window).resize(function() {
+        if (! g_reinit_lock) reinit();
+    });
+
+    RED   = hex2rgb($('#g-colors').css('--red'));
+    GREEN = hex2rgb($('#g-colors').css('--green'));
+    BLUE  = hex2rgb($('#g-colors').css('--blue'));
+
+    reinit();
+}
+
+function reinit() {
+    g_reinit_lock = true;
+
+    WIDTH  = $('#g-webgl').css('width');
+    HEIGHT = $('#g-webgl').css('height');
+
+    g_canvas = $('#webgl');
+    g_canvas.attr('width'  , WIDTH);
+    g_canvas.attr('height' , HEIGHT);
+
+    g_reinit_lock = false;
+}
+
+
+function main() {
+    init();
+
+    var canvas = g_canvas[0];
+
+    var gl = getWebGLContext(g_canvas[0]);
+    if (! gl) {
+        console.log('Failed to get the rendering context for WebGL');
+        return;
+    }
+
+    if (! initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log('Failed to intialize shaders.');
+        return;
+    }
+
+    gl.lineWidth(2.0);
+    render(gl);
+}
+
+function render(gl) {
+    clear(gl);
+    
+    var circle = new Circle([0.0, 0.0, 0.0], 0.75);
+    render_ngon(gl , circle.toNGon(100) , RED   , false);
+    render_ngon(gl , circle.toNGon(  8) , GREEN , false);
+    render_ngon(gl , circle.toNGon(  4) , BLUE  , false);
+}
+
+
+function flatten(arr) {
+    var res = new Float32Array(arr.length * 3);
+    for (var i = 0; i < arr.length; i++) {
+        res[3 * i]     = arr[i][0];
+        res[3 * i + 1] = arr[i][1];
+        res[3 * i + 2] = arr[i][2];
+    }
+    return res;
+}
+
+var Circle = function(center, radius) {
+    this.center = center;
+    this.radius = radius;
+};
+
+Circle.prototype = {
+    toNGon: function(n) {
+        var sliceAngle = 2 * Math.PI / n;
+
+        var res = [];
+        for (var i = 0; i < n; i++) {
+            if (n % 2 == 0) {  // 'n' is even
+                var x = this.radius * Math.cos(i * sliceAngle);
+                var y = this.radius * Math.sin(i * sliceAngle);
+            } else {
+                var y = this.radius * Math.cos(i * sliceAngle);
+                var x = this.radius * Math.sin(i * sliceAngle);
+            }
+            res[i] = [x, y, 0.0];
+        }
+
+        return res;
+    }
+};
+
+
+function clear(gl) {
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
+function render_ngon(gl, vertices, color, fill) {
+    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+
+    var u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
+    gl.uniform4f(u_FragColor, color[0], color[1], color[2], color[3]);
+
+    var vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.DYNAMIC_DRAW);
+    gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(a_Position);
+
+    if (fill)
+        gl.drawArrays(gl.TRIANGLE_FAN, 0, vertices.length);
+    else
+        gl.drawArrays(gl.LINE_LOOP, 0, vertices.length);
+}
 
 var VSHADER_SOURCE =
     'attribute vec4 a_Position;\n' +
-    'uniform float u_PointSize;' +
     'void main() {\n' +
     '  gl_Position = a_Position;\n' +
-    '  gl_PointSize = u_PointSize;\n' +
     '}\n';
 
 var FSHADER_SOURCE =
@@ -19,238 +139,24 @@ var FSHADER_SOURCE =
     '}\n';
 
 
-var g_canvas = $('#webgl');
-
-var g_mouseFocus = false;
-var g_pointSize = POINT_SIZE;
-$('#point-size-label').text(g_pointSize);
-$('#point-size-slider').attr('value', g_pointSize);
-
-var g_buildingLine = false;
-var g_polyLines = new PolyLines();
-
-
-function main() {
-    var canvas = g_canvas[0];
-
-    var gl = getWebGLContext(g_canvas[0]);
-    if (! gl) {
-        console.log('Failed to get the rendering context for WebGL');
-        return;
-    }
-
-    $('#polylines-reset').click(function() {
-        console.log('Resetting the application to initial state...');
-
-        g_pointSize = POINT_SIZE;
-        $('#point-size-slider').val(g_pointSize).trigger('change');
-
-        g_polyLines = new PolyLines();
-        $('#polyline-controls').html('<a> PolyLine Controls </a>').attr('hidden', 'true');
-        renderPolyLines(gl, g_pointSize);
-    });
-
-    $('#point-size-slider').on('change mousemove', function() {
-        g_pointSize = $('#point-size-slider').val();
-        $('#point-size-label').text(g_pointSize);
-        renderPolyLines(gl, g_pointSize);
-    });
-
-    if (! initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
-        console.log('Failed to intialize shaders.');
-        return;
-    }
-
-    g_canvas.mouseenter(function() {
-        g_mouseFocus = true;
-        console.log('Canvas has mouse focus: ' + g_mouseFocus);
-    });
-
-    g_canvas.mouseout(function() {
-        g_mouseFocus = false;
-        console.log('Canvas has mouse focus: ' + g_mouseFocus);
-        renderPolyLines(gl, g_pointSize);
-    });
-
-    g_canvas.mousedown(function(ev) {
-        switch (ev.which) {
-        case 1:
-            click(gl, getMouseXY(ev));
-            break;
-        case 3:
-            rightClick(gl, getMouseXY(ev));
-            break;
-        }
-    });
-
-    g_canvas.mousemove(function(ev) {
-        renderPolyLines(gl, g_pointSize, getMouseXY(ev));
-    });
-
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
-
-    gl.lineWidth(3.0);
-}
-
-
-function getMouseXY(ev) {
-    var x = ev.clientX;  // x coordinate of a mouse pointer
-    var y = ev.clientY;  // y coordinate of a mouse pointer
-    var rect = ev.target.getBoundingClientRect();
-
-    var canvas = g_canvas;
-
-    x = ((x - rect.left) - canvas.width() / 2)/(canvas.width() / 2);
-    y = (canvas.height() / 2 - (y - rect.top))/(canvas.height() / 2);
-
-    return [x, y];
-}
-
-
-function renderPolyLines(gl, pointSize, mouseXY) {
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
-
-    g_polyLines.map(function(i, polyLine) {
-        renderPolyLine(gl, polyLine, pointSize);
-    });
-    if (g_mouseFocus) {
-        renderPolyLine(gl, g_polyLines.current, pointSize, mouseXY);
-    } else {
-        renderPolyLine(gl, g_polyLines.current, pointSize);
-    }
-}
-
-function renderPolyLine(gl, polyLine, pointSize, mouseXY) {
-    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-    if (a_Position < 0) {
-        console.log('Failed to get the storage location of a_Position');
-        return;
-    }
-
-    var u_PointSize = gl.getUniformLocation(gl.program, 'u_PointSize');
-    if (! u_PointSize) {
-        console.log('Failed to get the storage location of u_PointSize');
-        return;
-    }
-    gl.uniform1f(u_PointSize, pointSize);
-
-    var u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
-    if (! u_FragColor) {
-        console.log('Failed to get the storage location of u_FragColor');
-        return;
-    }
-    var color = polyLine.getColor();
-    gl.uniform4f(u_FragColor, color[0], color[1], color[2], color[3]);
-
-    var vertexBuffer = gl.createBuffer();
-    if (! vertexBuffer) {
-        console.log('Failed to create the buffer object');
-        return;
-    }
-
-    var buffer = polyLine.flatten(mouseXY);
-    var count = buffer.length / 2;
-
-    if (count === 0)
-        return;
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, buffer, gl.DYNAMIC_DRAW);
-    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(a_Position);
-    gl.drawArrays(gl.POINTS, 0, count);
-    gl.drawArrays(gl.LINE_STRIP, 0, count);
-}
-
-
-function click(gl, mouseXY) {
-    g_buildingLine = true;
-    
-    var x = mouseXY[0];
-    var y = mouseXY[1];
-
-    console.log("Recieved click: " + point2string(x, y));
-
-    polyLine = g_polyLines.current;
-    polyLine.pushPoint(x, y);
-
-    renderPolyLines(gl, g_pointSize, mouseXY);
-}
-
-function rightClick(gl, mouseXY) {
-    var x = mouseXY[0];
-    var y = mouseXY[1];
-
-    console.log("Recieved right-click: " + point2string(x, y));
-
-    if (! g_buildingLine)
-        return;
-    g_buildingLine = false;
-
-    var j = g_polyLines.makeNew();
-    var points = g_polyLines.at(j).getPoints();
-    for (var i = 0; i < points.length; i++) {
-        var point = points[i];
-        console.log("g_polyLines[" + j + "][" + i + "]: " + point2string(point[0], point[1]));
-    }
-
-    var polyLineControl = `
-        <div id="polyline-control-${j}" class="control">
-	  <a> ${j} </a>
-	  <button class="remove" type="button">
-            <i class="fas fa-times"></i>
-          </button>
-	  <button class="red" type="button">
-            <code>RED</code>
-          </button>
-	  <button class="white" type="button">
-            <code>WHITE</code>
-          </button>
-	  <button class="blue" type="button">
-            <code>BLUE</code>
-          </button>
-	</div>
-    `;
-    $('#polyline-controls').removeAttr('hidden').append(polyLineControl);
-
-    polyLineControl = $('#polyline-control-' + j);
-    polyLineControl.children('button.remove').first().click(function() {
-        console.log('Removing PolyLine ' + j + '...');
-
-        $('#polyline-control-' + j).remove();
-        g_polyLines.remove(j);
-        renderPolyLines(gl, g_pointSize);
-
-        if (g_polyLines.count === 0) {
-            console.log('PolyLine count is 0, hiding controls...');
-            $('#polyline-controls').attr('hidden', 'true');
-        }
-    });
-    polyLineControl.children('button.red').first().click(function() {
-        console.log('Changing color of PolyLine ' + j + '...');
-
-        g_polyLines.at(j).setColor(RED);
-        renderPolyLines(gl, g_pointSize);
-    });
-    polyLineControl.children('button.white').first().click(function() {
-        console.log('Changing color of PolyLine ' + j + '...');
-        
-        g_polyLines.at(j).setColor(WHITE);
-        renderPolyLines(gl, g_pointSize);
-    });
-    polyLineControl.children('button.blue').first().click(function() {
-        console.log('Changing color of PolyLine ' + j + '...');
-
-        g_polyLines.at(j).setColor(BLUE);
-        renderPolyLines(gl, g_pointSize);
-    });
-
-    renderPolyLines(gl, g_pointSize);
-}
-
-
 function point2string(x, y) {
     return "{" + x.toString() + ", " + y.toString() + "}";
+}
+
+function hex2rgb(hex) {
+    // 'hexToRgb' function copied from 'https://stackoverflow.com/questions/5623838'
+    function hexToRgb(hex) {
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+        return result ? {
+            r: parseInt(result[1], 16),
+            g: parseInt(result[2], 16),
+            b: parseInt(result[3], 16)
+        } : null;
+    }
+
+    var rgb = hexToRgb(hex.trim());
+    rgb = [rgb.r, rgb.g, rgb.b, 1.0];
+    for (var i = 0; i < rgb.length; i++)
+        rgb[i] /= 255;
+    return rgb;
 }
